bccc9d19f71774e76571c99d6e8b103a9b12dec4
z0mbiebrad <bradleyt.evans@gmail.com>
2024-11-14 01:31:42 -0500

    update contact

--- resources/views/components/contact.blade.php
+++ resources/views/components/contact.blade.php
@@ -8,7 +8,7 @@
     <div id="contact" class="relative isolate bg-gray-900">
         <div class="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
             <div class="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
-                <div class="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
+                <div class="mx-auto lg:mx-0 lg:max-w-lg">
                     <div class="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden ring-1 ring-white/5 lg:w-1/2">
                         <svg class="absolute inset-0 size-full stroke-gray-700 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
                             aria-hidden="true">
@@ -63,8 +63,8 @@
                 </div>
             </div>
             <form action="#" method="POST" class="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
-                <div class="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
-                    <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                <div class="mx-auto lg:mr-0 lg:max-w-lg w-4/5">
+                    <div class="my-8 flex-col flex gap-x-8 gap-y-6 sm:grid-cols-2">
                         <div>
                             <label for="first-name" class="block text-sm/6 font-semibold text-white">First name</label>
                             <div class="mt-2.5">
