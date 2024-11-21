bc4acce3d79ba5b4bd4b537efe4e6b849c933e53
z0mbiebrad <bradleyt.evans@gmail.com>
2024-11-21 09:05:56 -0500

    blog-work

--- resources/views/vendor/prezet/components/header.blade.php
+++ resources/views/vendor/prezet/components/header.blade.php
@@ -2,36 +2,12 @@
     class="sticky top-0 z-50 flex flex-none flex-wrap items-center justify-between bg-white px-4 py-5 shadow-md shadow-gray-900/5 transition duration-500 sm:px-6 lg:px-8"
 >
     <div class="relative flex flex-grow basis-0 items-center">
-        <button
-            aria-label="Menu"
-            class="mr-4 rounded-lg p-1.5 hover:bg-gray-100 active:bg-gray-200 lg:hidden"
-            x-on:click="showSidebar = ! showSidebar"
-        >
-            <svg
-                class="h-6 w-6 text-gray-600"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-            >
-                <line x1="4" x2="20" y1="12" y2="12"></line>
-                <line x1="4" x2="20" y1="6" y2="6"></line>
-                <line x1="4" x2="20" y1="18" y2="18"></line>
-            </svg>
-        </button>
-
         <a
             aria-label="Home"
-            href="{{ route('prezet.index') }}"
-            class="inline-block flex items-center space-x-2"
+            href="{{ route('home') }}"
+            class="flex items-center space-x-2"
         >
             <x-prezet::logo />
-            <span class="text-2xl font-bold text-gray-900">
-                {{ strtoupper(config('app.name')) }}
-            </span>
         </a>
     </div>
     <div
@@ -54,5 +30,12 @@
                 ></path>
             </svg>
         </a>
+        <button
+            aria-label="Menu"
+            class="rounded-lg p-1.5 hover:bg-gray-100 active:bg-gray-200 lg:hidden"
+            x-on:click="showSidebar = ! showSidebar"
+        >
+            <x-thumbprint />
+        </button>
     </div>
 </header>
