8bda7cca5e99b0d816ef230a34ab4b11c4b61cde
z0mbiebrad <bradleyt.evans@gmail.com>
2024-11-21 22:47:41 -0500

    GH-14: Fixes

--- resources/views/vendor/prezet/components/sidebar.blade.php
+++ resources/views/vendor/prezet/components/sidebar.blade.php
@@ -5,10 +5,10 @@
     class="fixed inset-0 z-100 flex h-full items-start overflow-y-auto bg-gray-900/50 pr-10 backdrop-blur lg:hidden"
 >
     <div
-        class="min-h-full w-full max-w-xs bg-white px-4 pb-12 pt-24 sm:px-6"
+        class="min-h-full w-full max-w-xs px-4 pb-12 pt-24 sm:px-6"
         x-on:click.outside="showSidebar = false"
     >
-        <x-prezet::nav :nav="$nav" />
+        <x-prezet::mobile-menu :nav="$nav" />
     </div>
 </div>
 
