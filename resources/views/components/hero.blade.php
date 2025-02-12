8a21d19eff98b93f78ab6903d516661e0926a9ef
z0mbiebrad <bradleyt.evans@gmail.com>
2025-02-12 06:20:08 -0500

    Changed dark background color opacity from 10 - 40, and headwind autosrted classes

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,34 +1,34 @@
 <div id="about" class="relative isolate overflow-hidden pt-[4.5rem]">
-    <img src="{{ asset('images/back2.jpg') }}" alt="" class="dark:inline hidden absolute inset-0 -z-10 h-full w-full object-cover">
-    <img src="{{ asset('images/sun.jpg') }}" alt="" class="inline dark:hidden absolute inset-0 -z-10 h-full w-full object-cover">
-    <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+    <img src="{{ asset('images/back2.jpg') }}" alt="" class="absolute inset-0 hidden object-cover w-full h-full dark:inline -z-10">
+    <img src="{{ asset('images/sun.jpg') }}" alt="" class="absolute inset-0 inline object-cover w-full h-full dark:hidden -z-10">
+    <div class="absolute inset-x-0 overflow-hidden -top-40 -z-10 transform-gpu blur-3xl sm:-top-80" aria-hidden="true">
         <div class="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
         </div>
     </div>
-    <div class="flex flex-wrap dark:text-white bg-white/60 dark:bg-black/10 sm:justify-center text-black">
-        <div class="w-full sm:w-11/12 mb-10 max-w-5xl">
-            <div class="container mx-auto h-full">
+    <div class="flex flex-wrap text-black dark:text-white bg-white/60 dark:bg-black/40 sm:justify-center">
+        <div class="w-full max-w-5xl mb-10 sm:w-11/12">
+            <div class="container h-full mx-auto">
                 <img src="{{ asset('images/family.png') }}"
-                    alt="" class="w-full h-48 my-16 object-cover object-top sm:h-80">
+                    alt="" class="object-cover object-top w-full h-48 my-16 sm:h-80">
                 <header class="container h-full">
                     <div class="w-full">
-                        <h1 class="text-4xl sm:text-5xl lg:text-6xl font-bold mx-auto w-fit">Hi! I'm <span class="dark:text-indigo-400 text-orange-400">Bradley
+                        <h1 class="mx-auto text-4xl font-bold sm:text-5xl lg:text-6xl w-fit">Hi! I'm <span class="text-orange-400 dark:text-indigo-400">Bradley
                                 Evans</span>. <br>
-                            It's nice to meet <span class="dark:text-indigo-400 text-orange-400">you</span>!</h1>
-                        <div class="w-3/4 my-12 mx-auto h-1 dark:bg-indigo-400 bg-orange-400"></div>
-                        <p class="text-xl mb-12 w-fit mx-auto font-semibold px-5">I am a full stack web
+                            It's nice to meet <span class="text-orange-400 dark:text-indigo-400">you</span>!</h1>
+                        <div class="w-3/4 h-1 mx-auto my-12 bg-orange-400 dark:bg-indigo-400"></div>
+                        <p class="px-5 mx-auto mb-12 text-xl font-semibold w-fit">I am a full stack web
                             developer skilled in Laravel, TailwindCSS, and responsive design. I create efficient,
                             user-friendly web applications with clean, maintainable code. Open to opportunities where I
                             can apply my technical expertise in a collaborative setting.</p>
-                        <div class="mt-10 font-semibold flex items-center justify-center gap-x-6 dark:text-white">
+                        <div class="flex items-center justify-center mt-10 font-semibold gap-x-6 dark:text-white">
                             <a href="#examples"
-                                class="border-b-2 border-black dark:border-white text-md flex gap-x-1">
+                                class="flex border-b-2 border-black dark:border-white text-md gap-x-1">
                                 <x-circle-stack />
                                 Examples
                             </a>
                             <a href="#services"
-                                class="border-b-2 border-black dark:border-white text-md flex gap-x-1">
+                                class="flex border-b-2 border-black dark:border-white text-md gap-x-1">
                                 Services
                                 <x-command />
                             </a>
