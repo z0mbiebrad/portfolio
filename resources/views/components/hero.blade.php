714702eea9b096de00cf40d716bbcf2240452da7
z0mbiebrad <bradleyt.evans@gmail.com>
2025-02-13 13:18:35 -0500

    changed background opacity, added icons for my stack tech, and made them all link to the respective websites

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -6,31 +6,45 @@
             style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
         </div>
     </div>
-    <div class="flex flex-wrap text-black dark:text-white bg-white/60 dark:bg-black/40 sm:justify-center">
+    <div class="flex flex-wrap text-black dark:text-white bg-white/70 dark:bg-black/40 sm:justify-center">
         <div class="w-full max-w-5xl mb-10 sm:w-11/12">
             <div class="container h-full mx-auto">
                 <img src="{{ asset('images/family.png') }}"
-                    alt="" class="object-cover object-top w-full h-48 my-16 sm:h-80">
+                    alt="" class="object-cover object-top w-full h-48 my-16 sm:h-80 rounded-md shadow-xl">
                 <header class="container h-full">
                     <div class="w-full">
-                        <h1 class="mx-auto text-4xl font-bold sm:text-5xl lg:text-6xl w-fit">Hi! I'm <span class="text-orange-400 dark:text-indigo-400">Bradley
+                        <h1 class="mx-auto text-4xl font-bold sm:text-5xl lg:text-6xl w-fit">Hi! I'm <span class="text-orange-600 dark:text-indigo-400">Bradley
                                 Evans</span>. <br>
-                            It's nice to meet <span class="text-orange-400 dark:text-indigo-400">you</span>!</h1>
-                        <div class="w-3/4 h-1 mx-auto my-12 bg-orange-400 dark:bg-indigo-400"></div>
-                        <p class="px-5 mx-auto mb-12 text-xl font-semibold w-fit">I am a full stack web
-                            developer skilled in Laravel, TailwindCSS, and responsive design. I create efficient,
-                            user-friendly web applications with clean, maintainable code. Open to opportunities where I
-                            can apply my technical expertise in a collaborative setting.</p>
+                            It's nice to meet <span class="text-orange-600 dark:text-indigo-400">you</span>!</h1>
+                        <div class="w-3/4 h-1 mx-auto my-12 bg-orange-600 dark:bg-indigo-400"></div>
+                        <div class="grid grid-cols-2 sm:grid-cols-4 place-items-center mb-6">
+                            <a href="https://tailwindcss.com/" target="_blank" class="w-full h-16 hover:-rotate-3 group hover:scale-105 transition-all ease-out duration-300 text-center flex flex-col font-bold items-center justify-center">
+                                <x-tailwind class="m-2"/>
+                            </a>
+                            <a href="https://alpinejs.dev" target="_blank" class="w-full h-16 hover:-rotate-3 group hover:scale-105 transition-all ease-out duration-300 text-center flex flex-col font-bold items-center justify-center">
+                                <x-alpine class="m-2"/>
+                            </a>
+                            <a href="https://laravel.com/" target="_blank" class="w-full h-16 hover:-rotate-3 group hover:scale-105 transition-all ease-out duration-300 text-center flex flex-col font-bold items-center justify-center">
+                                <x-laravel class="m-1"/>
+                            </a>
+                            <a href="https://alpinejs.dev" target="_blank" class="w-full h-16 hover:-rotate-3 group hover:scale-105 transition-all ease-out duration-300 text-center flex flex-col font-bold items-center justify-center">
+                                <x-livewire class="m-2"/>
+                            </a>
+                        </div>
+
+                        <p class="max-w-4xl px-5 mx-auto mb-12 text-xl w-fit">
+                           I’m a <span class="font-bold">Full-Stack Developer</span> specializing in the TALL stack with a strong focus on problem-solving, critical thinking, and user experience. My approach is simple: make it work, then make it great.  I’m eager to join a stable development team where I can learn, collaborate, and grow, bringing adaptability, attention to detail, and a relentless drive to solve problems.
+                        </p>
                         <div class="flex items-center justify-center mt-10 font-semibold gap-x-6 dark:text-white">
                             <a href="#examples"
-                                class="flex border-b-2 border-black dark:border-white text-md gap-x-1">
-                                <x-circle-stack />
-                                Examples
+                                class="flex text-sm md:text-base lg:text-lg gap-x-1">
+                                <x-command />
+                                    Projects
                             </a>
                             <a href="#services"
-                                class="flex border-b-2 border-black dark:border-white text-md gap-x-1">
-                                Services
-                                <x-command />
+                                class="flex text-base lg:text-lg gap-x-1">
+                                Contact
+                                <x-chat />
                             </a>
                         </div>
                     </div>
